8bdff2d1806a0951d73b0f42c482bd23c5d58514
blackchestnut <alexander.kalinichev@gmail.com>
2021-12-20 18:31:53 +0300

    Add changelog dir

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  title: 'myBudget',
+  title: 'Мой бюджет',
   lang: 'ru-RU',
   themeConfig: {
     // logo: 'https://2.mybudget.ws/favicon-144.png'
+
+    sidebar: [
+      {
+        text: 'История изменений',
+        link: '/changelog/README.md',
+        children: [
+          '2021-12-19-выбор-графика-и-период-сохраняется.md',
+          '2021-12-10-возможность-задать-свой-интервал-в-отчетах.md',
+        ],
+      },
+    ],
   }
 }
